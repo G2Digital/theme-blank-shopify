7f4b02a06bda78d5b903204ec667193dee470597
Vinicius Grando <vinicius@g2digital.com.br>
2025-07-02 12:02:15 -0300

    [FEAT] Logo with image and new header

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,15 @@ export default defineConfig({
   plugins: [shopify(), tailwindcss()],
 
   // Configurar Tunnel para mapeamento de CSS em ambiente de personalização Shopify.
-  // server: {
-  //   tunnel: true,
-  //   cors: {
-  //     origin: [
-  //       /^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\])(?::\d+)?$/,
-  //       'https://dominio.myshopify.com'
-  //     ]
-  //   }
-  // },
+  server: {
+    tunnel: true,
+    cors: {
+      origin: [
+        /^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\])(?::\d+)?$/,
+        'https://cecbra-com.myshopify.com'
+      ]
+    }
+  },
 
   build: {
     rollupOptions: {
