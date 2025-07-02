3c4a60345566080d5c551b410f37a2cd977ece01
Vinicius Grando <vinicius@g2digital.com.br>
2025-07-02 09:57:53 -0300

    [FIRST] Commit

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Config} */
+export default {
+  singleQuote: true,
+  semi: false,
+  printWidth: 80,
+  trailingComma: 'none',
+  plugins: ['@shopify/prettier-plugin-liquid', 'prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './frontend/entrypoints/theme.css',
+  overrides: [
+    {
+      files: '*.liquid',
+      options: {
+        parser: 'liquid-html',
+        singleQuote: false,
+        printWidth: 120
+      }
+    }
+  ]
+}
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import shopify from 'vite-plugin-shopify'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [shopify(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].min.js',
+        chunkFileNames: '[name].[hash].min.js',
+        assetFileNames: '[name].[hash].min[extname]'
+      }
+    }
+  }
+})
