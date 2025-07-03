164fd7d2dee964d57b96121f76c0ede5aee27a30
Vinicius Grando <vinicius@g2digital.com.br>
2025-07-02 21:45:01 -0300

    [FEAT] Header

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
 import tailwindcss from '@tailwindcss/vite'
-
 import dotenv from 'dotenv'
+
 dotenv.config()
+
+const shopifyUrls = process.env.VITE_SHOPIFY_URL
+  ? process.env.VITE_SHOPIFY_URL.split(',').map((url) => url.trim())
+  : []
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [shopify(), tailwindcss()],
 
-  // Configurar Tunnel para mapeamento de CSS em ambiente de personalização Shopify.
   server: {
     tunnel: true,
     cors: {
       origin: [
         /^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\])(?::\d+)?$/,
-        process.env.VITE_SHOPIFY_URL
+        ...shopifyUrls
       ]
     }
   },
