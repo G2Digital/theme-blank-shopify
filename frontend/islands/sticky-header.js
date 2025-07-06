cefc70e5100fa5c227450a5f52a1fea00b16b610
Vinicius Grando <vinicius@g2digital.com.br>
2025-07-06 15:14:59 -0300

    [FEAT] Header transparent options

--- frontend/islands/sticky-header.js
+++ frontend/islands/sticky-header.js
@@ -1,9 +1,11 @@
 class StickyHeader extends window.HTMLElement {
   connectedCallback() {
     this.header = document.getElementById('shopify-section-header')
+    this.headerElement = this.header.querySelector('header')
     this.headerBounds = {}
     this.currentScrollTop = 0
     this.preventReveal = false
+    this.isFixed = this.headerElement.classList.contains('fixed')
 
     this.onScrollHandler = this.onScroll.bind(this)
     this.hideHeaderOnScrollUp = () => {
@@ -29,6 +31,38 @@ class StickyHeader extends window.HTMLElement {
 
     if (this.predictiveSearch && this.predictiveSearch.isOpen) return
 
+    // Se o header é fixo, usamos uma lógica diferente
+    if (this.isFixed) {
+      this.handleFixedHeader(scrollTop)
+    } else {
+      this.handleStickyHeader(scrollTop)
+    }
+
+    this.currentScrollTop = scrollTop
+  }
+
+  handleFixedHeader(scrollTop) {
+    // Para header fixo, ocultamos após 100px de scroll para baixo
+    if (scrollTop > this.currentScrollTop && scrollTop > 100) {
+      if (this.preventHide) return
+      window.requestAnimationFrame(this.hideFixed.bind(this))
+    } else if (scrollTop < this.currentScrollTop && scrollTop > 100) {
+      if (!this.preventReveal) {
+        window.requestAnimationFrame(this.revealFixed.bind(this))
+      } else {
+        window.clearTimeout(this.isScrolling)
+        this.isScrolling = setTimeout(() => {
+          this.preventReveal = false
+        }, 66)
+        window.requestAnimationFrame(this.hideFixed.bind(this))
+      }
+    } else if (scrollTop <= 100) {
+      window.requestAnimationFrame(this.resetFixed.bind(this))
+    }
+  }
+
+  handleStickyHeader(scrollTop) {
+    // Lógica original para header sticky
     if (
       scrollTop > this.currentScrollTop &&
       scrollTop > this.headerBounds.bottom
@@ -43,20 +77,37 @@ class StickyHeader extends window.HTMLElement {
         window.requestAnimationFrame(this.reveal.bind(this))
       } else {
         window.clearTimeout(this.isScrolling)
-
         this.isScrolling = setTimeout(() => {
           this.preventReveal = false
         }, 66)
-
         window.requestAnimationFrame(this.hide.bind(this))
       }
     } else if (scrollTop <= this.headerBounds.top) {
       window.requestAnimationFrame(this.reset.bind(this))
     }
-
-    this.currentScrollTop = scrollTop
   }
 
+  // Métodos para header fixo
+  hideFixed() {
+    this.headerElement.classList.add(
+      '-translate-y-full',
+      'transition-transform'
+    )
+  }
+
+  revealFixed() {
+    this.headerElement.classList.add('transition-transform')
+    this.headerElement.classList.remove('-translate-y-full')
+  }
+
+  resetFixed() {
+    this.headerElement.classList.remove(
+      '-translate-y-full',
+      'transition-transform'
+    )
+  }
+
+  // Métodos originais para header sticky
   hide() {
     this.header.classList.add('-translate-y-full', 'sticky', 'top-0')
   }
